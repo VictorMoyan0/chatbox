ae0da8ef4af9d606ac9f05123028db1164231482
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-01 12:03:00 -0300

    add routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from 'react-router-dom'
+
 function Navbar() {
+    const navigate = useNavigate();
     return(
         <>
-
+            <nav>
+                <ul>
+                    <li onClick={() => navigate("/")}>Home</li>
+                    <li onClick={() => navigate("/about")}>Sobre Nosotros</li>
+                    <li onClick={() => navigate("/chat")}>Chat</li>
+                </ul>
+            </nav>
         </>
     )
 }
