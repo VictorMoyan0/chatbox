3f9960bec90cd69adcf8d1a6c2dadf9830e03080
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-01 11:41:38 -0300

    change import of api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {  useEffect, useState } from "react"
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import Chat from './components/Chat.jsx'
 import Home from './components/Home.jsx'
 import Navbar from './components/Navbar.jsx'
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar.jsx'
 function App() {
   return (
     <>
-
+      <Chat />
     </>
   )
 }
