92695485012129bbce728a276b3b636279d180f6
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-01 11:36:32 -0300

    fix import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import {  useEffect, useState } from "react"
 import { GoogleGenAI } from "@google/genai";
-import Chat from '../components/Chat.jsx'
-import Home from '../components/Home.jsx'
-import Navbar from '../components/Navbar.jsx'
+import Chat from './components/Chat.jsx'
+import Home from './components/Home.jsx'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
     <>
-      
+
     </>
   )
 }
