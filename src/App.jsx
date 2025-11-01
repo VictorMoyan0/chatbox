24c6904e6afb148b98114a6fd5494dd8fb4e4024
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-01 16:50:44 -0300

    import style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import {  useEffect, useState } from "react"
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import './styles/general.css'
 import './styles/home.css'
 import Home from './components/Home.jsx'
 import Navbar from './components/Navbar.jsx'
