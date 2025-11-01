f6482dc04f6ed6c2015b61767a25b7dd03bbe7a6
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-01 11:27:36 -0300

    add imports of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import {  useEffect, useState } from "react"
+import { GoogleGenAI } from "@google/genai";
+import Chat from '../components/Chat.jsx'
+import Home from '../components/Home.jsx'
+import Navbar from '../components/Navbar.jsx'
 
 function App() {
   return (
