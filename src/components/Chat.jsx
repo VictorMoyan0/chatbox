e8559185c3676a2f52bf51cbc85872c7737dd24f
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-02 10:20:11 -0300

    fix route of import

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { GoogleGenAI } from "@google/genai";
-import "./Chat.css";
+import "../styles/chat.css";
 
 const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const ai = new GoogleGenAI({ apiKey });
