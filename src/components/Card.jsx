bb6843b3d54660cd4f3bfbbb5f9c35e02ec25574
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-05 16:49:03 -0300

    click on card navigate a route specified

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 function Card({icon: Icon, title, description, color, path}){
     const navigate = useNavigate();
     return(
-        <div className={`feature-card feature-${color}`} onClick={onClick}>
+        <div className="feature-card" onClick={() => navigate(path)}>
             <Icon className="feature-icon" style={{ color }} />
             <h3 className="feature-title">{title}</h3>
             <p className="feature-description">{description}</p>
