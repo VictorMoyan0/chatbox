b6039c239bd6f96164222d323a933557773605c4
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-05 10:37:35 -0300

    add import

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Users, Code, Shield, Terminal } from 'lucide-react';
-import './about.css';
-
+import '../styles/about.css'
  
 function About() {
     const technologies = [
