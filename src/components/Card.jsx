bf0c64597e579f1a148068175811173c5e45ab4f
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-05 16:45:00 -0300

    import useNavigate

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,6 @@
-function Card({icon: Icon, title, description, color, onClick}){
+import { useNavigate } from "react-router-dom";
+function Card({icon: Icon, title, description, color, path}){
+    const navigate = useNavigate();
     return(
         <div className={`feature-card feature-${color}`} onClick={onClick}>
             <Icon className="feature-icon" style={{ color }} />
