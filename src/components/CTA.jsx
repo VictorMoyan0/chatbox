f50244a5433070f675d60d0c8670e9ee7861e6d5
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-05 15:56:20 -0300

    fix redirection to /threats

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,9 @@
-function CTA({ onClick }){
+function CTA(){
     return (
         <div className="cta">
             <h2 className="cta-title">¿Necesitas Ayuda?</h2>
             <p className="cta-text">Nuestro asistente está listo para responder tus preguntas sobre sistemas operativos.</p>
-            <button className="cta-button" onClick={onClick}>Hablar con el Asistente</button>
+            <button className="cta-button" onClick={() => navigate("/threats")}>Hablar con el Asistente</button>
         </div>
     );
 }
